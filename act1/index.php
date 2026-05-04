2773d41be2f028c3bf45b1609d2379c2b9e34874
GeneAndreiManacop <geneandreimanacop1@gmail.com>
2026-05-04 10:20:50 +0800

    Refactor code structure for improved readability and maintainability

--- act1/index.php
+++ act1/index.php
@@ -13,65 +13,150 @@
         $phoneNum = "(+63) 912-345-6789";
         $email = "gemanacop@fit.edu.ph";
         $location = "Cainta, Philippines";
-        $role = "Software Developer"
+        $role = "Software Developer";
+        $shs_school = "Cainta Catholic College";
+        $shs_year = "2022 - 2024";
+        $jhs_school = "Cainta  Catholic College";
+        $jhs_year = "2018 - 2022";
+        $gs_school = "Cainta Catholic College";
+        $gs_year = "2011 - 2018";
+        $profileSummary = "Aspiring Software Developer and current student at FEU Institute of Technology, specializing in Web and Mobile Applications. Passionate about building functional and user-centric applications with a strong foundation in database management and various programming languages.";
+        $language1 = "English";
+        $language2 = "Filipino";
+        $skill1 = "Frontend Development";
+        $skill2 = "Mobile App Architecture";
+        $skill3 = "Database Management";
+        $skill4 = "Version Control (Git)";
+        
+        $certification1_name = "IT Specialist - Databases";
+        $certification2_name = "IT Specialist Java";
+        $certification3_name = "IT Specialist - JavaScript";
+        $certification4_name = "IT Specialist - Python";
+        $certification_provider = "Pearson VUE Certiport";
+        $certification_deadline_2030 = "Until 2030";
+        $certification_deadline_2031 = "Until 2031";
+        
+        // I used arrays to make it easier. I apologize if this isn't allowed.
+        $certificates = [
+            ["IT Specialist - Databases", "Pearson VUE Certiport", "until 2030"],
+            ["IT Specialist - Java", "Pearson VUE Certiport", "until 2030"],
+            ["IT Specialist - JavaScript", "Pearson VUE Certiport", "until 2031"],
+            ["IT Specialist - Python", "Pearson VUE Certiport", "until 2031"]
+        ]
     ?>
 
     <main>
         <section class="left-section">
+            <img src="profile_picture.jpg" alt="" class="profile-picture">
             <div class="info">
-                <h2>Contact</h2>
-                <hr>
-
-                <h3>Address</h3>
-                <p><?= $location?></p>
-                
-                <h3>Phone</h3>
-                <p><?= $phoneNum?></p>
-
-                <h3>Email</h3>
-                <p><?= $email?></p>
+                <div>
+                    <h2>Contact</h2>
+                    <hr>
+                </div>
+                <div>
+                    <h3>Address</h3>
+                    <p><?= $location?></p>  
+                </div>
+                <div>
+                    <h3>Phone</h3>
+                    <p><?= $phoneNum?></p>
+                </div>
+                <div>
+                    <h3>Email</h3>
+                    <p><?= $email?></p>
+                </div>
             </div>
             <div class="info list">
                 <h2>Languages</h2>
                 <hr>
                 <ul>
-                    <li>English</li>
-                    <li>Filipino</li>
+                    <li><?= $language1 ?></li>
+                    <li><?= $language2 ?></li>
+                </ul>
+            </div>
+            <div class="info list">
+                <h2>Skills</h2>
+                <hr>
+                <ul>
+                    <li><?= $skill1 ?></li>
+                    <li><?= $skill2 ?></li>
+                    <li><?= $skill3 ?></li>
+                    <li><?= $skill4 ?></li>
                 </ul>
             </div>
         </section>
         <section class="right-section">
-            <h1><?= $fullName; ?></h1>
-            <h3 class="role"><?= $role; ?></h3>
+            <h1 class="name"><?= $fullName; ?></h1>
+            <h2 class="role"><?= $role; ?></h3>
             <br>
             <div class="info">
                 <h2>Profile</h2>
                 <hr>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio eaque in rerum porro reprehenderit illo dolorum consequatur expedita asperiores, ea earum aliquam, molestiae ex similique magnam. Veritatis similique iusto enim!</p>
+                <p class="profile-summary"><?= $profileSummary ?></p>
             </div>
 
-            <div class="info education">
+            <div class="info">
                 <h2>Educational Attainment</h2>
                 <hr>
-                <div>
-                    <p class="school">Senior High School</h6>
-                    <div>
-                        <span>Cainta Catholic College</span>
-                        <span>2025 - 2026</span>
+                <div class="attainments-list">
+                    <div class="attainment-item">
+                        <p>Senior High School</p>
+                        <div>
+                            <span><?= $shs_school; ?></span>
+                            <span><?= $shs_year ?></span>
+                        </div>
+                    </div>
+                    <div class="attainment-item">
+                        <p>Junior High School</p>
+                        <div>
+                            <span><?= $jhs_school; ?></span>
+                            <span><?= $jhs_year ?></span>
+                        </div>
+                    </div>
+                    <div class="attainment-item">
+                        <p>Grade School</p>
+                        <div>
+                            <span><?= $gs_school; ?></span>
+                            <span><?= $gs_year ?></span>
+                        </div>
                     </div>
                 </div>
-                <div>
-                    <p class="school">Junior High School</h6>
-                    <div>
-                        <span>Cainta Catholic College</span>
-                        <span>2025 - 2026</span>
-                    </div>
-                </div>
-                <div>
-                    <p class="school">Grade School</h6>
-                    <div>
-                        <span>Cainta Catholic College</span>
-                        <span>2025 - 2026</span>
+            </div>
+
+            <!-- I used arrays to make my coding easier. I apologize if this is not allowed. -->
+            <div class="info">
+                <h2>Certifications</h2>
+                <hr>
+                <div class="info">
+                    <div class="attainments-list">
+                        <div class='attainment-item'>
+                            <p><?= $certification1_name ?></p>
+                            <div>
+                                <span><?= $certification_provider ?></span>
+                                <span><?= $certification_deadline_2030 ?></span>
+                            </div>
+                        </div> 
+                        <div class='attainment-item'>
+                            <p><?= $certification2_name ?></p>
+                            <div>
+                                <span><?= $certification_provider ?></span>
+                                <span><?= $certification_deadline_2030 ?></span>
+                            </div>
+                        </div> 
+                        <div class='attainment-item'>
+                            <p><?= $certification3_name ?></p>
+                            <div>
+                                <span><?= $certification_provider ?></span>
+                                <span><?= $certification_deadline_2031 ?></span>
+                            </div>
+                        </div> 
+                        <div class='attainment-item'>
+                            <p><?= $certification4_name ?></p>
+                            <div>
+                                <span><?= $certification_provider ?></span>
+                                <span><?= $certification_deadline_2031 ?></span>
+                            </div>
+                        </div> 
                     </div>
                 </div>
             </div>
