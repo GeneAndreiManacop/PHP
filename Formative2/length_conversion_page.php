d3136203253ec42d187019140d88b753d793e127
Gene Andrei Manacop <geneandreimanacop1@gmail.com>
2026-05-04 11:01:19 +0800

    Add length conversion page with initial metric conversion table

---
+++ Formative2/length_conversion_page.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Length Conservation Page</title>
+    <style>
+        table, th, td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+        th {
+            background-color: yellow;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        $millimetres_per_centimetre = 10;
+        $centimetres_per_decimetre = 10;
+        $centimetres_per_metre = 100;
+        $metres_per_kilometre = 1000;
+        $inch_per_foot = 12
+        $feet_per_yard = 3;
+        $yards_per_chain = 22;
+        $yards_per_furlong = 220;
+        $chains_per_furlong = 10;
+        $yards_per_mile = 1760;
+        $furlongs_per_mile = 8;
+
+    ?>
+    <main>
+        <table>
+            <tr>
+                <th colspan='4'>Metric Conversions</th>
+            </tr>
+            <tr>
+                <td>Metric Conversions</th>
+            </tr>
+        </table>
+    </main>
+</body>
+</html>
