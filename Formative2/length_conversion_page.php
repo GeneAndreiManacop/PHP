00b8425eb39d637d9cf91b6090cbe132e42f3d5d
Gene Andrei Manacop <geneandreimanacop1@gmail.com>
2026-05-04 11:05:31 +0800

    Separated style to CSS file

--- Formative2/length_conversion_page.php
+++ Formative2/length_conversion_page.php
@@ -4,15 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Length Conservation Page</title>
-    <style>
-        table, th, td {
-            border: 1px solid black;
-            border-collapse: collapse;
-        }
-        th {
-            background-color: yellow;
-        }
-    </style>
+    <link rel="stylesheet" href="length_conversion_page_style.css">
 </head>
 <body>
     <?php
@@ -20,7 +12,7 @@
         $centimetres_per_decimetre = 10;
         $centimetres_per_metre = 100;
         $metres_per_kilometre = 1000;
-        $inch_per_foot = 12
+        $inch_per_foot = 12;
         $feet_per_yard = 3;
         $yards_per_chain = 22;
         $yards_per_furlong = 220;
@@ -32,10 +24,15 @@
     <main>
         <table>
             <tr>
-                <th colspan='4'>Metric Conversions</th>
+                <th colspan='6'>Metric Conversions</th>
             </tr>
             <tr>
-                <td>Metric Conversions</th>
+                <td class="unit">1 centimetre</th>
+                <td class="equal-cell">=</th>
+                <td class="unit"><?= $millimetres_per_centimetre?></th>
+                <td class="unit">1 centimetre</th>
+                <td class="equal-cell">=</th>
+                <td class="unit"><?= $millimetres_per_centimetre?></th>
             </tr>
         </table>
     </main>
