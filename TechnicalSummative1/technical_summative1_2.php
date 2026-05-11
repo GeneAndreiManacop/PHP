dca4be78af5e8a1958d995c9361948d8ee12e449
Gene Andrei Manacop <geneandreimanacop1@gmail.com>
2026-05-11 11:37:14 +0800

    Added initial student registration form HTML and PHP structure

---
+++ TechnicalSummative1/technical_summative1_2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Registration Form</title>
+</head>
+<body>
+    <?php
+        $firstName = "GeneAndreiManacop";
+        $lastName = "Manacop";
+        $middleName = "Estrada";
+        $birthDate = "2005-11-18";
+        $gender = "Male";
+        $nationality = "Filipino";
+        
+    ?>
+    <form action="">
+        <label for="usernamename"></label>
+        <input type="text" name="username" id="" value=<?=$username?>>
+    </form>
+</body>
+</html>
