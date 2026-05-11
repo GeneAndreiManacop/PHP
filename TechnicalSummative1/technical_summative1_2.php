6668d6552e3eaa1b61f44a17a5fb4cf8f2651a3d
Gene Andrei Manacop <geneandreimanacop1@gmail.com>
2026-05-11 23:09:14 +0800

    Updated document title to "Multiplication Table"

--- TechnicalSummative1/technical_summative1_2.php
+++ TechnicalSummative1/technical_summative1_2.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Multiplication Table</title>
     <link rel="stylesheet" href="technical_summative1_2_style.css">
 </head>
 <body>
