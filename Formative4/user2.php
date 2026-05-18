9200510644988e9edbb563ce9ba409c2ea520578
Gene Andrei Manacop <geneandreimanacop1@gmail.com>
2026-05-18 21:34:49 +0800

    Webpage of user2 to be include

---
+++ Formative4/user2.php
@@ -0,0 +1,44 @@
+<img src="ferguson.jpg" alt="">
+<div class="user-description">
+    <h2>Felix Ferguson</h2>
+    <p>A frontend developer focused on building fast, responsive, and accessible web experiences. He loves experimenting with new CSS frameworks and modern JavaScript libraries to bring creative user interfaces to life.</p>
+</div>
+<div class="user-info">
+    
+    <div class="field">
+        <span class="field-name">Phone Number:</span>
+        <span class="field-value">(666) 371-1625</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Email:</span>
+        <span class="field-value">felix.ferguson@example.com</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Gender:</span>
+        <span class="field-value">Male</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Age:</span>
+        <span class="field-value">29</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Birth Date:</span>
+        <span class="field-value">August 17, 1996</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Address:</span>
+        <span class="field-value">657 College St</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Hobbies:</span>
+        <span class="field-value">PC gaming, photography, and collecting mechanical keyboards</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Occupation:</span>
+        <span class="field-value">Web Developer</span>
+    </div>
+    <div class="field">
+        <span class="field-name">College Degree:</span>
+        <span class="field-value">Bachelor of Science in Information Technology</span>
+    </div>
+</div>
