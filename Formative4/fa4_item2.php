7001d52fe7652c3892ebb4c048f7f396931c1d14
Gene Andrei Manacop <geneandreimanacop1@gmail.com>
2026-05-18 19:48:15 +0800

    Define 20 random names of array, create header and row for each name, each with 6 columns.

--- Formative4/fa4_item2.php
+++ Formative4/fa4_item2.php
@@ -7,5 +7,51 @@
     <title>FA4 Item 2</title>
 </head>
 <body>
+    <?php
+        // 20 names in the array
+        $arrayOfNames = array(
+            "Alice",
+            "Bob",
+            "Charlie",
+            "David",
+            "Eve",
+            "Frank",
+            "Grace",
+            "Heidi",
+            "Ivan",
+            "Judy",
+            "Karl",
+            "Leo",
+            "Mallory",
+            "Nina",
+            "Oscar",
+            "Peggy",
+            "Quentin",
+            "Rupert",
+            "Sybil",
+            "Trent"
+        );
+    ?>
+    <header>List of names</header>
+    <main>
+        <div class="row header">
+            <div class="column">Name</div>
+            <div class="column">Number of Characters</div>
+            <div class="column">Uppercase first character</div>
+            <div class="column">Replace vowels with @</div>
+            <div class="column">Check position of character "a"</div>
+            <div class="column">Reverse name</div>
+        </div>
+        <?php foreach ($arrayOfNames as $name): ?>
+            <div class="row data">
+                <div class="column"><?= $name ?></div>
+                <div class="column"><?= strlen($name) ?></div>
+                <div class="column"><?= ucfirst($name) ?></div>
+                <div class="column"><?= str_replace(['a', 'e', 'i', 'o', 'u'], '@', $name) ?></div>
+                <div class="column"><?= strpos($name, 'a') !== false ? strpos($name, 'a') : 'N/A' ?></div>
+                <div class="column"><?= strrev($name) ?></div>
+            </div>
+        <?php endforeach; ?>
+    </main>
 </body>
 </html>
