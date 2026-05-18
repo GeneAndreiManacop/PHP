14ecd8f554686cee68ccddade5d152b1b56f7657
Gene Andrei Manacop <geneandreimanacop1@gmail.com>
2026-05-18 21:34:53 +0800

    Webpage of user3 to be include

---
+++ Formative4/user3.php
@@ -0,0 +1,44 @@
+<img src="flores.jpg" alt="">
+<div class="user-description">
+    <h2>Leona Flores</h2>
+    <p>A detail-oriented analyst who loves turning messy datasets into clear, actionable visual stories. She specializes in data visualization, writing complex SQL queries, and optimizing platform performance.</p>
+</div>
+<div class="user-info">
+    
+    <div class="field">
+        <span class="field-name">Phone Number:</span>
+        <span class="field-value">(579) 428-7349</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Email:</span>
+        <span class="field-value">leona.flores@example.com</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Gender:</span>
+        <span class="field-value">Female</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Age:</span>
+        <span class="field-value">30</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Birth Date:</span>
+        <span class="field-value">August 29, 1995</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Address:</span>
+        <span class="field-value">7539 Marsh Ln</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Hobbies:</span>
+        <span class="field-value">Reading sci-fi novels, playing chess, and playing the acoustic guitar</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Occupation:</span>
+        <span class="field-value">Data Analyst</span>
+    </div>
+    <div class="field">
+        <span class="field-name">College Degree:</span>
+        <span class="field-value">Bachelor of Science in Data Science</span>
+    </div>
+</div>
