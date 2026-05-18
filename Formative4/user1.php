4092a393fe52867336a67986733898ed34192ff1
Gene Andrei Manacop <geneandreimanacop1@gmail.com>
2026-05-18 21:34:45 +0800

    Webpage of user1 to be include

---
+++ Formative4/user1.php
@@ -0,0 +1,44 @@
+<img src="hopkins.jpg" alt="">
+<div class="user-description">
+    <h2>Marc Hopkins</h2>
+    <p>A software developer with a passion for open-source projects. He has been coding for over 10 years and enjoys contributing to the community.</p>
+</div>
+<div class="user-info">
+    
+    <div class="field">
+        <span class="field-name">Phone Number:</span>
+        <span class="field-value">(849) 695-4931</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Email:</span>
+        <span class="field-value">marc.hopkins@example.com</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Gender:</span>
+        <span class="field-value">Male</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Age:</span>
+        <span class="field-value">23</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Birth Date:</span>
+        <span class="field-value">July 20, 2002</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Address:</span>
+        <span class="field-value">8151 E Sandy Lake Rd</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Hobbies:</span>
+        <span class="field-value">Coding, hiking, and cooking</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Occupation:</span>
+        <span class="field-value">Software Engineer</span>
+    </div>
+    <div class="field">
+        <span class="field-name">College Degree:</span>
+        <span class="field-value">Bachelor of Science in Computer Science</span>
+    </div>
+</div>
