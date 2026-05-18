57a086e717679f692f8a65c8cec1ae8d6e5dcd23
Gene Andrei Manacop <geneandreimanacop1@gmail.com>
2026-05-18 10:57:17 +0800

    Define HTML foundation and table elements

---
+++ Formative3/fa3_item3.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>FA3 Item 2</title>
+</head>
+<body>
+
+    <table>
+        <th colspan=2>
+        </th>
+        <tr>
+            <td>
+                Addition
+            </td>
+            <td>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Subtraction
+            </td>
+            <td>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Multiplication
+            </td>
+            <td>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Division
+            </td>
+            <td>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
