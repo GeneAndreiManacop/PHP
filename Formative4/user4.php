479b897db9eea6129d94ad0bb0d25f6d7279f0c2
Gene Andrei Manacop <geneandreimanacop1@gmail.com>
2026-05-18 21:34:56 +0800

    Webpage of user4 to be include

---
+++ Formative4/user4.php
@@ -0,0 +1,44 @@
+<img src="ramirez.jpg" alt="">
+<div class="user-description">
+    <h2>Teresa Ramirez</h2>
+    <p>A creative designer passionate about human-centered design principles. She spends her days mapping out user journeys, building interactive mobile wireframes, and conducting user test interviews to craft intuitive interfaces.</p>
+</div>
+<div class="user-info">
+    
+    <div class="field">
+        <span class="field-name">Phone Number:</span>
+        <span class="field-value">(653) 594-7373</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Email:</span>
+        <span class="field-value">teresa.ramirez@example.com</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Gender:</span>
+        <span class="field-value">Female</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Age:</span>
+        <span class="field-value">22</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Birth Date:</span>
+        <span class="field-value">September 11, 2003</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Address:</span>
+        <span class="field-value">4162 Dogwood Ave</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Hobbies:</span>
+        <span class="field-value">Digital painting, traveling, and visiting local coffee shops</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Occupation:</span>
+        <span class="field-value">UI/UX Designer</span>
+    </div>
+    <div class="field">
+        <span class="field-name">College Degree:</span>
+        <span class="field-value">Bachelor of Science in Interactive Design</span>
+    </div>
+</div>
