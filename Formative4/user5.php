63e9a52076a244cb478a8f5c519847de6e349da4
Gene Andrei Manacop <geneandreimanacop1@gmail.com>
2026-05-18 21:35:00 +0800

    Webpage of user5 to be include

---
+++ Formative4/user5.php
@@ -0,0 +1,44 @@
+<img src="carpenter.jpg" alt="">
+<div class="user-description">
+    <h2>Roberta Carpenter</h2>
+    <p>An energetic software engineering graduate specializing in native iOS and Android application development. She is dedicated to writing clean, modular code and keeping up with the latest trends in mobile architecture.</p>
+</div>
+<div class="user-info">
+    
+    <div class="field">
+        <span class="field-name">Phone Number:</span>
+        <span class="field-value">(558) 274-6592</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Email:</span>
+        <span class="field-value">roberta.carpenter@example.com</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Gender:</span>
+        <span class="field-value">Female</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Age:</span>
+        <span class="field-value">27</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Birth Date:</span>
+        <span class="field-value">June 18, 1998</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Address:</span>
+        <span class="field-value">4650 Valley View Ln</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Hobbies:</span>
+        <span class="field-value">Biking, learning foreign languages, and pixel art animation</span>
+    </div>
+    <div class="field">
+        <span class="field-name">Occupation:</span>
+        <span class="field-value">Mobile App Developer</span>
+    </div>
+    <div class="field">
+        <span class="field-name">College Degree:</span>
+        <span class="field-value">Bachelor of Science in Computer Engineering</span>
+    </div>
+</div>
