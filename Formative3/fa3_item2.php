5da50f0a719f3be3a60d4f91fe51c4602f4b6faf
Gene Andrei Manacop <geneandreimanacop1@gmail.com>
2026-05-18 10:45:58 +0800

    Add computation and table of arithmetic results

--- Formative3/fa3_item2.php
+++ Formative3/fa3_item2.php
@@ -6,6 +6,69 @@
     <title>FA3 Item 2</title>
 </head>
 <body>
-    
+    <?php 
+        $numArray = array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        $sum = null;
+        $difference = null;
+        $product = null;
+        $quotient = null;
+
+        foreach ($numArray as $num) {
+            if (is_null($sum) && is_null($difference) && is_null($product) && is_null($quotient)) {
+                $sum = $num;
+                $difference = $num;
+                $product = $num;
+                $quotient = $num;
+            } else {
+                $sum += $num;
+                $difference -= $num;
+                $product *= $num;
+                $quotient /= $num;
+            }
+        }
+    ?>
+
+    <table>
+        <th>
+            Array list: <?php
+                foreach ($numArray as $num) {
+                    echo $num.", ";
+                }
+            ?>
+        </th>
+        <tr>
+            <td>
+                Addition
+            </td>
+            <td>
+                <?= $sum; ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Subtraction
+            </td>
+            <td>
+                <?= $difference; ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Multiplication
+            </td>
+            <td>
+                <?= $product; ?>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Division
+            </td>
+            <td>
+                <?= $quotient; ?>
+            </td>
+        </tr>
+    </table>
 </body>
 </html>
