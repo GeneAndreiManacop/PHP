389524b3f50f2a9aab4e24a2662261456690038d
Gene Andrei Manacop <geneandreimanacop1@gmail.com>
2026-05-18 10:57:50 +0800

    Link to external CSS file

--- Formative3/fa3_item3.php
+++ Formative3/fa3_item3.php
@@ -4,18 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="stylesheet" href="fa3_item2_style.css">
     <title>FA3 Item 2</title>
 </head>
 <body>
 
     <table>
         <th colspan=2>
+            My parameter values: <?php
+                echo $param1 . ", " . $param2 .", " . $param3
+            ?>
         </th>
         <tr>
             <td>
                 Addition
             </td>
             <td>
+                <?= add($param1, $param2, $param3); ?>
             </td>
         </tr>
         <tr>
@@ -23,6 +28,7 @@
                 Subtraction
             </td>
             <td>
+                <?= subtract($param1, $param2, $param3); ?>
             </td>
         </tr>
         <tr>
@@ -30,6 +36,7 @@
                 Multiplication
             </td>
             <td>
+                <?= multiply($param1, $param2, $param3); ?>
             </td>
         </tr>
         <tr>
@@ -37,6 +44,7 @@
                 Division
             </td>
             <td>
+                <?= divide($param1, $param2, $param3); ?>
             </td>
         </tr>
     </table>
