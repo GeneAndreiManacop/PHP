245826c7165ed6c79f9f854956bfac9af6a0fa35
Gene Andrei Manacop <geneandreimanacop1@gmail.com>
2026-05-18 19:47:05 +0800

    HTML foundation

---
+++ Formative4/fa4_item2.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="fa4_item2_style.css">
+    <title>FA4 Item 2</title>
+</head>
+<body>
+</body>
+</html>
