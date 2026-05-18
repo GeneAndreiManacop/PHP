8002a7cf4a160173f1536f9b3e6281600e5d6d81
Gene Andrei Manacop <geneandreimanacop1@gmail.com>
2026-05-18 10:59:59 +0800

    Change document title

--- Formative3/fa3_item3.php
+++ Formative3/fa3_item3.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="fa3_item2_style.css">
-    <title>FA3 Item 2</title>
+    <link rel="stylesheet" href="fa3_item3_style.css">
+    <title>FA3 Item 3</title>
 </head>
 <body>
     <?php 
