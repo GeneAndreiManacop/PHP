371045491f58bf4e02c3260862df68e6c6cfd85f
Gene Andrei Manacop <geneandreimanacop1@gmail.com>
2026-05-19 11:33:18 +0800

    Create main page for resume. Use include statements.

---
+++ TechnicalSummative2/TSA2_item3.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Resume</title>
+
+    <link rel="stylesheet" href="TSA2_item3_style.css">
+</head>
+
+<body>
+    <main class="resume-grid">
+        <div class="box profile-pic">
+            <img src="profile_picture.jpg" alt="">
+        </div>
+        <?php include("TSA2_item3_personal_info.php") ?>
+        <?php include("TSA2_item3_career_objective.php") ?>
+        <?php include("TSA2_item3_educational_attainment.php") ?>
+        <?php include("TSA2_item3_skills.php") ?>
+        <?php include("TSA2_item3_certifications.php") ?>
+    </main>
+</body>
+
+</html>
